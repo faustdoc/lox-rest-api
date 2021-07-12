d8628be5852b071f73ec99e012398f0620230167
grroz <zaf.mitx@gmail.com>
2021-07-12 21:55:43 +0300

    Wrapping server scaffolding

--- index.js
+++ index.js
@@ -6,11 +6,60 @@
 
 // Dependencies
 var http = require('http');
+var url = require('url');
+var strDecoder = require('string_decoder').StringDecoder;
+
+// Definitions
 var port = 3000;
 
 // Serve responds to all requests with a string
 var server = http.createServer((req, res) => {
-    res.end('Hello World');
+
+    // Get and parse url
+    var parsed = url.parse(req.url, true);
+
+    // Get path of the url 
+    var path = parsed.pathname;
+    var trimmed = path.replace(/^\/+|\/+$/g, '');
+    
+    // Get query string as an object 
+    var queryString = parsed.query;
+
+    // Obtain the method
+    var method = req.method.toLowerCase();
+
+    // Get the headers
+    var headers = req.headers;
+
+    // Get the payload, if any
+    var decoder = new strDecoder('utf-8');
+    var buffer = '';
+    
+    // Handle incoming stream 
+    req.on('data', (data) => {
+        buffer += decoder.write(data);
+    });
+
+    // Handle stream once it stops
+    req.on('end',() => {
+        buffer += decoder.end();
+
+        // Send response 
+        res.end('lx-res');
+
+        // Log path requested
+        log = {
+            request: trimmed,
+            query: queryString,
+            payload: buffer, 
+            method,
+            headers,
+        };
+        console.log(log);
+    });
+
+
+
 });
 
 // Start the serve and have it listen to the port 3000
